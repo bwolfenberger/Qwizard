184531a9a4267f45b3ad48f1516d67269da1dcc8
Benji <bwolfenberger@gmail.com>
2021-07-26 13:38:49 -0700

    added nav and small styling

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+    return(
+        <div>
+            <h1 className='title'>
+                <span>WebDev</span>News
+            </h1>
+        </div>
+    )
+}
+
+export default Header
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require('axios')
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  let financeUrl = `https://quizapi.io/api/v1/questions?apiKey=NO9367LnqoEXNu5hv33VZJbtftSxXPxaEcF1vxGb&limit=10&tags=HTML`
+    axios.get(financeUrl)
+    .then(apiRes => {
+      let financeData = apiRes.data
+      res.status(200).json({ financeData }) 
+  })
 }

ffb875247c1058288e354b86cef00e705df3e3a8
Benji <bwolfenberger@gmail.com>
2021-07-27 16:55:07 -0700

    added answer dispay and additional styling

--- components/Nav.js
+++ components/Nav.js
@@ -8,9 +8,6 @@ const Nav = () => {
                 <li>
                     <Link href='/'>Home</Link>
                 </li>
-                <li>
-                    <Link href='/about'>About</Link>
-                </li>
             </ul>
         </nav>
     )
--- pages/questions/[category]/index.js
+++ pages/questions/[category]/index.js
@@ -1,14 +1,25 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+
 import questionStyles from '../../../styles/Questions.module.css'
+
 const quizApiKey = process.env.QUIZ_API_KEY
 
+
+
 const questions = (questions) => {
+
+    const [showAnswer, setShowAnswer] = useState(false);
+    function toggle(){
+      setShowAnswer(!showAnswer);
+    }
+
     // const router = useRouter()
     // const { category } = router.query
 // console.log(questions)
 // console.log(questions.questions[0].answers)
 const ansArray = Object.values(questions.questions[0].correct_answers)
-const correctAnsIndexes = ansArray.map((ans, idx) => ans == 'true' ? idx: '').filter(String)
+const correctAnsIndexes = ansArray.map((ans, idx) => ans === 'true' ? idx: '').filter(String)
 correctAnsIndexes.map(index => {
     console.log(index + 1)
 })
@@ -16,14 +27,34 @@ correctAnsIndexes.map(index => {
 console.log(ansArray)
     return (
         <div>
-            <h2>{questions.category} Questions</h2>
+
+
+            <h2 className={questionStyles.grid}>{questions.category} Questions</h2>
             {questions.questions.map((question) => (
                 <div className={questionStyles.grid}>
-                    <h3>{question.question}</h3>
+
+
+                    <h3 className={questionStyles.title}>{question.question}</h3>
+
+
                         {Object.values(question.answers).map((answers, i) => (
                             answers !== null &&
                                 <a className={questionStyles.card}>{answers}</a>
                     ))}
+
+                    <button className={questionStyles.button} onClick={toggle}>Show Answers</button>
+                    <div style={{
+                        display: showAnswer?"block":"none"
+                    }}>
+                        {Object.values(question.answers).map((answers, i) => (
+                            i == (parseInt(Object.values(question.correct_answers).map((ans, idx) => ans === 'true' ? idx: '').filter(String))) &&
+                                <>
+                                    <a className={questionStyles.answer}>{answers}</a>
+                                </>
+                    ))}
+                    </div>
+
+
                 </div>
             ))}
         </div>

c7a217c0de688efe8ca54ec6b9c673bc507086da
Benji <bwolfenberger@gmail.com>
2021-09-02 13:19:56 -0700

    added button

--- pages/newQuiz.tsx
+++ pages/newQuiz.tsx
@@ -3,21 +3,24 @@ const NewQuiz = (props) => {
     return(
         <>  
             <h4>Create your own quiz</h4>
-                <form>
-                    <label>Quiz Title </label>
-                    <input type="quizTitle" value={props.quizTitle} onChange={(e) => props.setQuizTitle(e.target.value)} placeholder="Quiz Title" />
-                </form>
-                <form>
-                    <label>Question 1</label>
-                    <input type="quizQuestion" value={props.quizQuestion} onChange={(e) => props.setQuizQuestion(e.target.value)} placeholder="Question" />
-                </form>
-                <form>
-                    <label>Answers </label>
-                    <input type="answer1" value={props.answer1} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 1"/>
-                    <input type="answer2" value={props.answer2} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 2"/>
-                    <input type='answer3' value={props.answer3} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 3"/>
-                    <input type='answer4' value={props.answer4} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 4"/>
-                </form>
+            <form>
+                <label>Quiz Title </label>
+                <input type="quizTitle" value={props.quizTitle} onChange={(e) => props.setQuizTitle(e.target.value)} placeholder="Quiz Title" />
+            </form>
+            <form>
+                <label>Question 1</label>
+                <input type="quizQuestion" value={props.quizQuestion} onChange={(e) => props.setQuizQuestion(e.target.value)} placeholder="Question" />
+            </form>
+            <form>
+                <label>Answers </label>
+                <input type="answer1" value={props.answer1} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 1"/>
+                <input type="answer2" value={props.answer2} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 2"/>
+                <input type='answer3' value={props.answer3} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 3"/>
+                <input type='answer4' value={props.answer4} onChange={(e) => props.setAnswer(e.target.value)} placeholder="Answer 4"/>
+            </form>
+            <div className="create-btn">
+            <button id="createCard" onClick={() => props.handleJobCreate()}>Create Quiz</button>
+            </div>
         </>
     )
 }
